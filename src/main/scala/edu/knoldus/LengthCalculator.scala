d9eb85ed8212b6f563b379a08370d6f84f775df2
prakharsingh2818 <prakhar.singh@knoldus.com>
2022-01-28 23:44:40 +0530

    Code formatting

--- src/main/scala/edu/knoldus/LengthCalculator.scala
+++ src/main/scala/edu/knoldus/LengthCalculator.scala
@@ -2,10 +2,10 @@ package edu.knoldus
 
 import scala.annotation.tailrec
 
-class LengthCalculator{
+class LengthCalculator {
   @tailrec
   final def calculateLength[T](list: Iterable[T], accumulator: Int = 0): Int = {
-    if(list.isEmpty) accumulator
+    if (list.isEmpty) accumulator
     else calculateLength(list.tail, accumulator + 1)
   }
 }
